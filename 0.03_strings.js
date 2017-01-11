d8d78fff75ca521317dc995669d580402badfade
Lisa Hamilton <lisaqhamilton@gmail.com>
2017-01-11 11:59:32 -0500

    learning about strings

---
+++ 0.03_strings.js
@@ -0,0 +1,60 @@
+/*
+var firstName = "Lisa";
+var lastName = "Hamilton";
+console.log(firstName + " " + lastName);
+
+console.log(firstName + ": " + 55);//this is read as a string
+console.log(typeof firstName);//let's you know the data type ie. string,number
+
+// quotes?
+var maidenName = "Quiett";
+console.log(typeof maidenName);
+
+// Use a forward slash to let javascript know the statement has not ended (for example to include the
+//apostrophe as the string.
+//You can use single and double quotes 
+//For including actual quotes, use the single quotations for the string
+//then the double quotations for the actual quote
+var greetings = 'Lisa says "How\'s your day?"';
+console.log(greetings)
+
+//string methods
+console.log(greetings.length);
+console.log(greetings.toUpperCase());
+console.log(greetings.toLowerCase());
+
+//.split adds a space in between each 
+console.log(greetings.split());
+//.slice trunks the result by the number count you input.
+//For example, 0 would remove first spot.
+console.log(greetings.slice(0));
+var email = lisaqhamilton@gmail.com
+console.log(email.split("@"));
+*/
+
+var favFood 
+var weekDay
+favFood = "Pizza"
+weekDay = "Saturday"
+console.log("I love to eat " + favFood + " each week on " +weekDay);
+
+var carMake = "Chrysler";
+var carModel = "Suburban";
+var carYear = "2011";
+var statement = "I drive a " + carYear + " " +carMake + " " +carModel;
+	console.log(statement);
+
+var age
+age = 41
+if (age >= 21){
+		console.log("You're an adult, go grab a beer")
+	}
+	else if (age >=18){
+ 		console.log("You're an adult but not adult enough to drink");
+	}
+	else{
+		console.log("You're still a minor, too bad")
+	}
+
+
+	
